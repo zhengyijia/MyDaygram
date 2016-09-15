e0bf0d93591c862c455910526822d99f187027ff
zhengyijia <zhengyijia142@qq.com>
2016-09-15 11:33:36 +0800

    Initial commit

---
+++ app/src/main/java/com/oneplus/mydaygram/ShowPageActivity.java
@@ -0,0 +1,13 @@
+package com.oneplus.mydaygram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_page);
+    }
+}
